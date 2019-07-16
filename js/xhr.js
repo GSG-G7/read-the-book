698867b8b3847004a339bfeb87391f7a101c7f74
FadiAlamassi <alamassifadi@gmail.com>
2019-07-16 16:25:24 +0300

    adding google book key and search function to urls relates #4

--- js/xhr.js
+++ js/xhr.js
@@ -1,12 +1,14 @@
 const urls = {
     apiKeys : {
        moviedb: "88a88f263d1037a89abd98086e80504c",
+       bookdb:"AIzaSyCpKMgL4JrjDRVYbOUTaHTj587XvMi153Y",
     },
     moviedbSearch : (query) => `https://api.themoviedb.org/3/search/movie?api_key=${urls.apiKeys.moviedb}&language=en-US&query=${query}&page=1&include_adult=false`,
     moviedbDetails : (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbCredits : (id) => `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${urls.apiKeys.moviedb}`,
     moviedbVideos : (id) => `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbImageLink: (link) => `https://image.tmdb.org/t/p/w500`+link,
+    googleBookSearch: (query) => `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.goodreads}`
 } 
 
 function makeApiRequest(url, callback) {
@@ -19,4 +21,6 @@ function makeApiRequest(url, callback) {
     xhr.open('GET',url);
     xhr.send();
 }
-console.log(makeApiRequest(urls.moviedbSearch("me before you"),(x)=> console.log(x)))
+
+// console.log(makeApiRequest(urls.moviedbSearch("me before you"),(x)=> console.log(x)))
+console.log(makeApiRequest(urls.googleBookSearch("me before you"),(x)=> console.dir(JSON.parse(x))))
