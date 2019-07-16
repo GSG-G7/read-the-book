9c4a407e4a80a580b5c80b4584aea0d6d1d4a1ed
SajedaIsmail <eng.sajedaismail@gmail.com>
2019-07-16 15:51:14 +0300

    googlebookSearch function added to the urls pbject for book API Relates #4

--- js/xhr.js
+++ js/xhr.js
@@ -1,12 +1,15 @@
 const urls = {
     apiKeys : {
        moviedb: "88a88f263d1037a89abd98086e80504c",
+       bookdb: "AIzaSyCZxEaGwfrMVo6ZlbLR3_qWsaMYs1LEsR8",
     },
     moviedbSearch : (query) => `https://api.themoviedb.org/3/search/movie?api_key=${urls.apiKeys.moviedb}&language=en-US&query=${query}&page=1&include_adult=false`,
     moviedbDetails : (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbCredits : (id) => `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${urls.apiKeys.moviedb}`,
     moviedbVideos : (id) => `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbImageLink: (link) => `https://image.tmdb.org/t/p/w500`+link,
+    googleBookSearch:(query)=> `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.bookdb}`
+
 } 
 
 function makeApiRequest(url, callback) {
@@ -28,3 +31,4 @@ const moviesdb = {
     },
 }
 console.log(makeApiRequest(urls.moviedbSearch("me before you"),(x)=> console.log(x)))
+console.log(makeApiRequest(urls.googleBookSearch("me before you"),(x)=> console.log(x)))
