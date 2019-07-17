96f136527ec165fb9483819b9dc8d3a2a2734d63
FadiAlamassi <alamassifadi@gmail.com>
2019-07-17 09:34:49 +0300

    fix conflict relates #21

--- js/xhr.js
+++ js/xhr.js
@@ -1,47 +1,53 @@
 const urls = {
     apiKeys : {
        moviedb: "88a88f263d1037a89abd98086e80504c",
-       bookdb:"AIzaSyCpKMgL4JrjDRVYbOUTaHTj587XvMi153Y",
+       bookdb: "AIzaSyCZxEaGwfrMVo6ZlbLR3_qWsaMYs1LEsR8"
     },
     moviedbSearch : (query) => `https://api.themoviedb.org/3/search/movie?api_key=${urls.apiKeys.moviedb}&language=en-US&query=${query}&page=1&include_adult=false`,
     moviedbDetails : (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbCredits : (id) => `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${urls.apiKeys.moviedb}`,
     moviedbVideos : (id) => `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbImageLink: (link) => `https://image.tmdb.org/t/p/w500`+link,
-    googleBookSearch: (query) => `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.bookdb}`
+    youtubeLink: (key) => `https://www.youtube.com/watch?v=${key}`,
+    googleBookSearch:(query)=> `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.bookdb}`
 } 
 
 function makeApiRequest(url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (xhr.readyState === 4 && xhr.status === 200){
-            callback(xhr.responseText);
+            callback(xhr.response);
         }
     }
     xhr.open('GET',url);
+    xhr.responseType = 'json';
     xhr.send();
 }
-// search function
 const moviesdb = {
     search: function (title) {
-        makeApiRequest(urls.moviedbSearch(title),function(responseText){
-            return JSON.parse(responseText).results.slice(0,7);
+        makeApiRequest(urls.moviedbSearch(title),function(res){
+            res.results.slice(0,7);
         });
     },
     details: function(id) {
-        makeApiRequest(urls.moviedbDetails(id), function(responseText) {
-            let response = JSON.parse(responseText);
-            return {
-                overview: response.overview,
-                genre: response.genres,
-                rating: response.vote_average
+        makeApiRequest(urls.moviedbDetails(id), function(res) {
+            let obj = {
+                overview: res.overview,
+                genre: res.genres,
+                rating: res.vote_average
             };
         })
     },
     credits: function(id) {
-        makeApiRequest(urls.moviedbCredits(id), function(responseText) {
-            return JSON.parse(responseText).cast.slice(0,4);
+        makeApiRequest(urls.moviedbCredits(id), function(res) {
+             res.cast.slice(0,4);
+        });
+    },
+    video: function(id){
+        makeApiRequest(urls.moviedbVideos(id), function(res) {
+            return urls.youtubeLink(res.results[0].key);
         });
     }
-
 }
+moviesdb.video("296096");
+// console.log(makeApiRequest(urls.googleBookSearch('Me before you'),(x)))
