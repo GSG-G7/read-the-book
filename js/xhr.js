9f621aa66513a20a017ab2c73fcf9604b6e74bc4
FadiAlamassi <alamassifadi@gmail.com>
2019-07-17 09:25:04 +0300

    Creating the structure of html file and adding some style relates #21

--- js/xhr.js
+++ js/xhr.js
@@ -1,23 +1,14 @@
 const urls = {
     apiKeys : {
        moviedb: "88a88f263d1037a89abd98086e80504c",
-<<<<<<< HEAD
        bookdb:"AIzaSyCpKMgL4JrjDRVYbOUTaHTj587XvMi153Y",
-=======
-       bookdb: "AIzaSyCZxEaGwfrMVo6ZlbLR3_qWsaMYs1LEsR8",
->>>>>>> e06834d0fa2039175144e77a9f78d8c1e3ebd60b
     },
     moviedbSearch : (query) => `https://api.themoviedb.org/3/search/movie?api_key=${urls.apiKeys.moviedb}&language=en-US&query=${query}&page=1&include_adult=false`,
     moviedbDetails : (id) => `https://api.themoviedb.org/3/movie/${id}?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbCredits : (id) => `https://api.themoviedb.org/3/movie/${id}/credits?api_key=${urls.apiKeys.moviedb}`,
     moviedbVideos : (id) => `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${urls.apiKeys.moviedb}&language=en-US`,
     moviedbImageLink: (link) => `https://image.tmdb.org/t/p/w500`+link,
-<<<<<<< HEAD
-    googleBookSearch: (query) => `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.goodreads}`
-=======
-    googleBookSearch:(query)=> `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.bookdb}`
-
->>>>>>> e06834d0fa2039175144e77a9f78d8c1e3ebd60b
+    googleBookSearch: (query) => `https://www.googleapis.com/books/v1/volumes?q=${query}+intitle:${query}&key=${urls.apiKeys.bookdb}`
 } 
 
 function makeApiRequest(url, callback) {
@@ -30,11 +21,6 @@ function makeApiRequest(url, callback) {
     xhr.open('GET',url);
     xhr.send();
 }
-<<<<<<< HEAD
-
-// console.log(makeApiRequest(urls.moviedbSearch("me before you"),(x)=> console.log(x)))
-console.log(makeApiRequest(urls.googleBookSearch("me before you"),(x)=> console.dir(JSON.parse(x))))
-=======
 // search function
 const moviesdb = {
     search: function (title) {
@@ -58,5 +44,18 @@ const moviesdb = {
         });
     }
 }
-console.log(makeApiRequest(urls.googleBookSearch('Me before you'),(x)))
->>>>>>> e06834d0fa2039175144e77a9f78d8c1e3ebd60b
+
+const googleBook = {
+    search:function(query){
+        makeApiRequest(urls.googleBookSearch(query),(response)=>{
+            let res =JSON.parse(response);  
+            const title = res.items.slice(0,5)[0].volumeInfo.title;
+            const auther = res.items.slice(0,5)[0].volumeInfo.authors;
+            const description = res.items.slice(0,5)[0].volumeInfo.description;
+            const averageRating = res.items.slice(0,5)[0].volumeInfo.averageRating;
+            const image = res.items.slice(0,5)[0].volumeInfo.imageLinks.thumbnail;       
+        })
+    }
+}
+
+googleBook.search('Me before you');
